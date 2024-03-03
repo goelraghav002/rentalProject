05581d24c4b13fd996e5328a53665a1345916341
Akshansh <akshanshmodi2002@gmail.com>
2024-03-04 00:56:12 +0530

    🤑

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -29,11 +29,11 @@ export default function ListingItem({ listing }) {
             {listing.description}
           </p>
           <p className="text-slate-500 mt-2 font-semibold ">
-            $
+            Rs&nbsp;
             {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
-            {listing.type === "rent" && " / month"}
+            {listing.type === "rent" && "/month"}
           </p>
           <div className="text-slate-700 flex gap-4">
             <div className="font-bold text-xs">
