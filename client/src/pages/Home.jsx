2be0448d51718942e06a93f5a617fb0d95c20a13
Akshansh <akshanshmodi2002@gmail.com>
2024-03-04 00:04:14 +0530

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
 import ListingItem from "../components/ListingItem";
+import "./other.css";
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
@@ -48,23 +49,23 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-      <div className=" flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+      <div className=" flex flex-col gap-6 p-28 bgimage">
+        <h1 className="border-none text-white font-bold text-3xl lg:text-[40px]">
+          Discover Your <span className="text-white">Ideal</span>
           <br />
-          place with ease
+          Home effortlessly
         </h1>
-        <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
+        <div className="text-pink-400 text-xs sm:text-[19px] font-bold">
+          rentalBuddies is the best place to find your   <br />next perfect place to
           live.
-          <br />
-          We have a wide range of properties for you to choose from.
+       
+          
         </div>
         <Link
           to={"/search"}
-          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+          className="border-none text-xs sm:text-[15px] text-white font-bold hover:underline"
         >
-          Let's get started...
+          Let's Begin Your Search...
         </Link>
       </div>
 
