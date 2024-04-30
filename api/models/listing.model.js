5efb81fe44d678c24d904354150b36b2126ff41e
Raghav Goel <goelraghav002@gmail.com>
2024-05-01 03:40:14 +0530

    added create cintract

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -18,6 +18,10 @@ const listingSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    newId: {
+      type: Number,
+      required: true,
+    },
     discountPrice: {
       type: Number,
       required: true,
